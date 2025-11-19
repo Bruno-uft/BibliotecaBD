750c7adb9cb0875543ac7f9bd339cdca506ca22a
kayke002 <kaykezagopinheiro2020@gmail.com>
2025-11-18 21:30:19 -0300

    conexão com o banco de dados
    conexão com o banco de dados e um CRUD

---
+++ conexão.php
@@ -0,0 +1,13 @@
+<?php 
+
+$hostname = "localhost";
+$usuario = "root";
+$senha = "";
+$bancodedados = "biblioteca";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+if ($mysqli->connect_errno) {
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} 
+?>
